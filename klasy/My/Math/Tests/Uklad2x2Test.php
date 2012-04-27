51bec3da0f4949ad336415847e0f2a987a43732b
Włodzimierz Gajda <gajdaw@gajdaw.pl>
2012-04-27 14:50:11 +0200

    fix

--- klasy/My/Math/Tests/Uklad2x2Test.php
+++ klasy/My/Math/Tests/Uklad2x2Test.php
@@ -9,4 +9,30 @@ require_once realpath(__DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR
 class Uklad2x2Test extends \PHPUnit_Framework_TestCase
 {
 
+
+    /**
+     *
+     * @dataProvider getTestDet2Data
+     *
+     */
+    public function testDet2($m, $d)
+    {
+        $u = new Uklad2x2();
+        $this->assertEquals($u->det2($m), $d);
+    }
+
+    public function getTestDet2Data()
+    {
+        return array(
+            array(
+                array(array(1, 0), array(0, 1)),
+                array(array(1, 2), array(7, 10)),
+            ),
+            array(
+                1,
+                -15
+            )
+        );
+    }
+
 }
