30bf48a636924be9bf706c81f8137437854148d0
Włodzimierz Gajda <gajdaw@gajdaw.pl>
2012-04-27 14:46:14 +0200

    fix

---
+++ klasy/My/Math/Tests/Uklad2x2Test.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace My\Math\Tests;
+
+use My\Math\Uklad2x2;
+
+require_once realpath(__DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'Uklad2x2.php');
+
+class Uklad2x2Test extends \PHPUnit_Framework_TestCase
+{
+
+}
